8b2e8afbaf0b2cd161ff5435f981ac2a07013df0
raz-ezra <razezra.music@gmail.com>
2023-06-20 16:52:03 +0300

    fix verify

--- libs/nx/src/mocks.ts
+++ libs/nx/src/mocks.ts
@@ -2,9 +2,3 @@ import * as globby from 'globby';
 
 export const workspaceCwd = 'libs/nx/src/__fixtures__/nx-workspace';
 export const projectCwd = 'libs/nx/src/__fixtures__/nx-project';
-
-export const mockGlobby = () => {
-  jest
-    .spyOn(globby, 'globby')
-    .mockResolvedValue(['./proj1/project.json', './proj2/project.json']);
-};
